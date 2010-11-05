db6b1f02eb0d6b43a12481cd8d7ee90e63ad149b
Florian Echtler <floe@butterbrot.org>
2010-11-05 16:18:37 +0000

    bridge for Citron MTIR library (very beta)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/libtisch-1.1@49 28952012-f250-0410-9090-e835bad21ca5

---
+++ drivers/mtirbridge/mtirbridge.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <mtirdefs.h>
+#include <mtirctldll.h>
+
+int main ( int argc, char* argv[] ) {
+
+	std::cout << "MtirctlEnum: " << std::flush;
+	int num = MtirctlEnum();
+	std::cout << num << std::endl;
+	if (num == 0) return 0;
+
+	int dev = 0;
+	int run = 1;
+
+	bool res = MtirctlOpen( dev );
+	MtirctlStartScan( dev, NULL );
+
+	MTIR_GEOMETRY geo;
+	res = MtirctlGetGeometry( dev, &geo );
+
+	std::cout << "size: " << geo.sx << " " << geo.sy << std::endl;
+
+	MTIR_POSITION pos[64];
+	MTIR_SCANRES scanres;
+	scanres.pPosition = pos;
+
+	while (run) {
+
+		res = MtirctlGetScanResult( dev, &scanres );
+
+		if (res)
+			for (int i = 0; i < scanres.nPositions; i++)
+				std::cout << pos[i].id << " " << pos[i].x << " " << pos[i].y << std::endl;
+	}
+
+	res = MtirctlStopScan( dev );
+	res = MtirctlClose( dev );
+
+	return 0;
+}
+
