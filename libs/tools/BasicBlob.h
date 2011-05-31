fdbefd617de026c126062cec0a913be85e3de2c0
Florian Echtler <floe@butterbrot.org>
2011-05-31 16:29:25 +0000

    added new TUIO 2.0 type definitions (not yet enabled)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/libtisch-2.0@308 28952012-f250-0410-9090-e835bad21ca5

--- libs/tools/BasicBlob.h
+++ libs/tools/BasicBlob.h
@@ -23,6 +23,60 @@ enum InputType {
 	INPUT_TYPE_COUNT
 };
 
+/* same list with proper TUIO 2.0 types (not yet enabled):
+
+// generic names
+#define INPUT_TYPE_FINGER INPUT_TYPE_FINGER_RI
+#define INPUT_TYPE_HAND   INPUT_TYPE_HAND_RP
+#define INPUT_TYPE_FOOT   INPUT_TYPE_FOOT_R
+#define INPUT_TYPE_ANY    0xFFFFFFFF
+
+enum InputType {
+
+	INPUT_TYPE_UNKNOWN,
+
+	INPUT_TYPE_FINGER_RI, // right index finger
+	INPUT_TYPE_FINGER_RM, // right middle finger
+	INPUT_TYPE_FINGER_RR, // right ring finger
+	INPUT_TYPE_FINGER_RL, // right little finger
+	INPUT_TYPE_FINGER_RT, // right thumb
+
+	INPUT_TYPE_FINGER_LI, // left index finger
+	INPUT_TYPE_FINGER_LM, // left middle finger
+	INPUT_TYPE_FINGER_LR, // left ring finger
+	INPUT_TYPE_FINGER_LL, // left little finger
+	INPUT_TYPE_FINGER_LT, // left thumb
+
+	INPUT_TYPE_STYLUS,
+	INPUT_TYPE_LASER,
+	INPUT_TYPE_MOUSE,
+	INPUT_TYPE_TRACKBALL,
+	INPUT_TYPE_JOYSTICK,
+
+	INPUT_TYPE_WIIMOTE,
+	INPUT_TYPE_OBJECT,
+	INPUT_TYPE_TOKEN,
+
+	INPUT_TYPE_HAND_RP = 21, // right hand pointing
+	INPUT_TYPE_HAND_RO,      // right hand open
+	INPUT_TYPE_HAND_RC,      // right hand closed
+
+	INPUT_TYPE_HAND_LP,      // left hand pointing
+	INPUT_TYPE_HAND_LO,      // left hand open
+	INPUT_TYPE_HAND_LC,      // left hand closed
+
+	INPUT_TYPE_FOOT_R,
+	INPUT_TYPE_FOOT_L,
+
+	INPUT_TYPE_HEAD,
+	INPUT_TYPE_PERSON,
+
+	// always keep as last - must be <= 31
+	INPUT_TYPE_COUNT 
+};
+
+*/
+
 
 class TISCH_SHARED BasicBlob {
 
