8cd4d4ae9151616cf60d5ecc00d3fb8adb558a08
Florian Echtler <floe@butterbrot.org>
2011-11-09 14:26:23 +0000

    - use registered depth data - use swapData to avoid memcpy
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/libtisch@389 28952012-f250-0410-9090-e835bad21ca5

--- libs/simplecv/KinectImageSource.cc
+++ libs/simplecv/KinectImageSource.cc
@@ -98,8 +98,8 @@ void* kinecthandler( void* arg ) {
 	freenect_set_depth_callback( src->f_dev, depth_cb );
 	freenect_set_video_callback( src->f_dev, rgb_cb );
 
-	freenect_frame_mode dmode = freenect_find_depth_mode( FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_11BIT );
-	freenect_frame_mode vmode = freenect_find_video_mode( FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB   );
+	freenect_frame_mode dmode = freenect_find_depth_mode( FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_REGISTERED );
+	freenect_frame_mode vmode = freenect_find_video_mode( FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB        );
 
 	freenect_set_depth_mode( src->f_dev, dmode ); // assert(dmode.bytes == src->depthbuf[0]->size())
 	freenect_set_video_mode( src->f_dev, vmode ); // assert(vmode.bytes == src->videobuf[0]->size())
@@ -144,24 +144,18 @@ void KinectImageSource::getImage( IntensityImage& target ) const {
 	uint16_t* source = (uint16_t*)depthbuf[(curdb+1)%2]->getData();
 	uint8_t*  targ = (uint8_t*)target.getData();
 	for (int i = 0; i < width*height; i++) {
-		// the depth sensor is actually 11 bits - 2047 == too near/too far
-		uint8_t tmp = source[i] >> 3;
+		// the depth data is in mm, from 0 to ~8000 = 13 bits
+		uint8_t tmp = source[i] >> 5;
 		targ[i] = 255 - tmp; // remapping: 0 = no data, 254 = very close
 	}
 }
 
 void KinectImageSource::getImage( ShortImage& target ) const {
-//	target = *depthbuf;
-	uint16_t* source = (uint16_t*)depthbuf[(curdb+1)%2]->getData();
-	uint16_t* targ = (uint16_t*)target.getData();
-	for (int i = 0; i < width*height; i++) {
-		// the depth sensor is actually 11 bits - 2047 == too near/too far
-		targ[i] = (2047 - source[i]) << 5;
-	}
+	depthbuf[(curdb+1)%2]->swapData(target);
 }
 
 void KinectImageSource::getImage( RGBImage& target ) const {
-	target = *(videobuf[(curvb+1)%2]);
+	videobuf[(curvb+1)%2]->swapData(target);
 }
 
 
