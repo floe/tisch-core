b71ce22462ea988300b0dcf702c283cf9ecfba73
Florian Echtler <floe@butterbrot.org>
2012-02-16 15:57:28 +0000

    add missing functions (refactorization pending)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/libtisch@407 28952012-f250-0410-9090-e835bad21ca5

--- gispl/features/Path.cc
+++ gispl/features/Path.cc
@@ -60,6 +60,112 @@ Vector centroid( std::vector<Vector>& path ) {
 	return result*(1.0/n);
 }
 
+std::vector<Vector> rotate_path( std::vector<Vector>& path, double rad ) {
+
+	std::vector<Vector> result;
+	Vector center = centroid( path );
+
+	for (std::vector<Vector>::iterator p = path.begin(); p != path.end(); p++) {
+		Vector tmp = (*p - center); tmp.rotate(rad);
+		result.push_back( tmp + center );
+	}
+
+	return result;
+}
+
+std::vector<Vector> scale_path( std::vector<Vector>& path, double factor ) {
+
+	std::vector<Vector> result;
+	Vector bbox = bbox_dim( path );
+
+	for (std::vector<Vector>::iterator p = path.begin(); p != path.end(); p++)
+		result.push_back( Vector( p->x * (factor/bbox.x), p->y * (factor/bbox.y), 0 ) ); //p->z * (factor/bbox.z) ) );
+
+	return result;
+}
+
+std::vector<Vector> translate_path( std::vector<Vector>& path, Vector shift ) {
+
+	std::vector<Vector> result;
+	Vector center = centroid( path );
+
+	for (std::vector<Vector>::iterator p = path.begin(); p != path.end(); p++)
+		result.push_back( (*p - center) + shift );
+
+	return result;
+}
+
+double direction( std::vector<Vector>& path ) {
+	Vector center = centroid( path );
+	return atan2( center.y - path[0].y, center.x - path[0].x );
+}
+
+std::vector<Vector> vectorize( std::vector<Vector>& path ) {
+
+	std::vector<Vector> result;
+	double mag,sum = 0.0;
+
+	for (std::vector<Vector>::iterator p = path.begin(); p != path.end(); p++) {
+		result.push_back( *p );
+		sum += p->x * p->x + p->y * p->y;
+	}
+
+	mag = sqrt(sum);
+	for (std::vector<Vector>::iterator r = result.begin(); r != result.end(); r++) {
+		*r = *r * (1.0/mag);
+	}
+
+	return result;
+}
+
+std::vector<Vector> resample( std::vector<Vector>& path, int num_points ) {
+
+	std::vector<Vector> result; result.push_back(path[0]);
+	double interval = path_length( path ) / (num_points-1);
+	double total_dist = 0.0;
+
+	for (unsigned int i = 1; i < path.size(); i++) {
+		double dist = (path[i] - path[i-1]).length();
+		if ((total_dist+dist) >= interval) {
+			double qx = path[i-1].x + ((interval - total_dist) / dist) * (path[i].x - path[i-1].x);
+			double qy = path[i-1].y + ((interval - total_dist) / dist) * (path[i].y - path[i-1].y);
+			Vector q(qx,qy,0);
+			result.push_back( q );
+			//path.insert( &(path[i+1]), q ); // FIXME
+			total_dist = 0.0;
+		} else total_dist += dist;
+	}
+
+	if ((int)result.size() == num_points-1)
+		result.push_back( *(path.rbegin()) );
+
+	return result;
+}
+
+double protractor_distance( std::vector<Vector>& path1, std::vector<Vector>& path2 ) {
+	double a = 0.0;
+	double b = 0.0;
+	int n = path1.size();
+	for (int i = 0; i < n; i++) {
+		a += path1[i].x * path2[i].x + path1[i].y * path2[i].y;
+		b += path1[i].x * path2[i].y - path1[i].y * path2[i].x;
+	}
+	double angle = atan(b/a);
+	return acos( a*cos(angle) + b*sin(angle) );
+}
+
+void process_path( std::vector<Vector>& path ) {
+	// resample(path,64);
+	// rotate_path(path,direction);
+	// scale_path(path, 250.0);
+	// translate_path(path,Vector(0,0,0));
+	// vectorize(path);
+}
+
+
+
+
+// actual feature value calculation takes place below
 
 void Path::load( InputState& state ) {
 
