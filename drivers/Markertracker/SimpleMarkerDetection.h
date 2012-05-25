6978900ea20ac776b2ff56525c7333a3249a89af
Andreas Dippon <dippona@in.tum.de>
2012-05-25 12:01:36 +0000

    added Ubitrack Markertracker (will be used in our Markertracker in coming commits)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tisch/code/libtisch@424 28952012-f250-0410-9090-e835bad21ca5

---
+++ drivers/Markertracker/SimpleMarkerDetection.h
@@ -0,0 +1,24 @@
+
+#ifndef __UBITRACK_VISION_SIMPLEMARKERDETECTION_H_INCLUDED__
+#define __UBITRACK_VISION_SIMPLEMARKERDETECTION_H_INCLUDED__
+
+#define UBITRACK_EXPORT __declspec( dllimport )
+
+namespace Ubitrack { namespace Vision {
+
+struct SimpleMarkerInfo
+{
+	int ID; //Marker ID
+	cv::Point2f corners[4]; //Corners
+	std::vector<double> pos; //Pose Position
+	std::vector<double> rot; //Pose Rotation
+};
+
+/**
+ * Detect Markers in an IplImage and save them in a vector of SimpleMarkerInfo.
+ */
+UBITRACK_EXPORT void findMarkers(IplImage* img, std::vector<SimpleMarkerInfo>& simpleMarkers, std::vector<int>& markerList, IplImage* debugimg) throw();
+
+ } } 
+
+#endif
